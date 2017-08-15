d74013d0a08b9a8032ba4ca66819378b4158661f
Svarog <admin@brainode.com>
2017-08-15 20:14:05 +0300

    Init version of game manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    private List<PlayerManager> players;
+
+    #region MonoBehaviour
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            Debug.Log(players[0].GetRace());
+        }
+    }
+    #endregion
+
+    public static GameManager GetGameManager()
+    {
+        return instance;
+    }
+
+}
---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Race
+{
+    Spectator = 0,
+    Citizen = 1,
+    Fermer = 2
+}
+
+public class PlayerManager : MonoBehaviour
+{
+    private int foodResource,
+                equipResource,
+                specialResource;
+
+    private Race playerRace;
+
+    PlayerManager(Race race)
+    {
+        playerRace = race;
+    }
+    public int GetRace()
+    {
+        try
+        {
+            return (int) playerRace;
+        }
+        catch (NullReferenceException nExp)
+        {
+            Debug.Log("Race not setted");
+            return -1;
+        }
+    }
+}
