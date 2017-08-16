49f6ad0182a5b96e0e98586c390c191a8f80f716
nmaschinskiy <nmaschinskiy@mail.ru>
2017-08-16 15:13:00 +0400

    Create TileGrid and intermediate classes

---
+++ Assets/Scripts/SO/TileSO.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "TileSettings", menuName = "My Scriptable Objects/Tile Settings", order = 0)]
+public class TileSO : ScriptableObject
+{
+	[SerializeField] private Tile[] tiles;
+
+	public Tile GetBulletSettings(TileType type)
+	{
+		foreach (var tile in tiles)
+		{
+			if (tile.TileType == type)
+			{
+				return tile;
+			}
+		}
+
+		return tiles[0];
+	}
+}
---
+++ Assets/Scripts/TileGrid/TileGrid.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGrid
+{
+	/// <summary>
+	/// X count
+	/// </summary>
+	public int GridWidth { get; private set; }
+
+	/// <summary>
+	/// Z count
+	/// </summary>
+	public int GridLength { get; private set; }
+
+	private TileType[,] grid;
+	private Dictionary<TileType, Tile> tiles;
+
+	public TileGrid(int width, int length, TileType defaultTileType = TileType.Ground)
+	{
+		GridWidth = width;
+		GridLength = length;
+
+		CreateGrid(defaultTileType);
+	}
+
+	private void CreateGrid(TileType defaultTileType)
+	{
+		ReceiveTiles();
+
+		TileType tiletype = TileType.Ground;
+
+		if(tiles.ContainsKey(defaultTileType))
+		{
+			tiletype = defaultTileType;
+		}
+
+		grid = new TileType[GridWidth, GridLength];
+
+		for (int x = 0; x < GridWidth; x++)
+		{
+			for(int z = 0; z < GridLength; z++)
+			{
+				grid[x, z] = tiletype;
+			}
+		}
+	}
+
+	private void ReceiveTiles()
+	{
+		//TODO Reading from SceneManager
+	}
+}
