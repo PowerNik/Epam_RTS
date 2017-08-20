77be871d3232db389a982d367b486fa2859572bd
Svarog <admin@brainode.com>
2017-08-20 19:42:36 +0300

    Health Bar Init

---
+++ Assets/Scripts/HUD/HealthHUD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthHUD : MonoBehaviour
+{
+    private Transform CameraTransform;
+    void Start()
+    {
+        CameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+	void Update () {
+        //transform.rotation = Quaternion.LookRotation(CameraTransform.position.normalized);
+	    transform.rotation = Quaternion.LookRotation(Vector3.forward)*CameraTransform.rotation;
+    }
+}
