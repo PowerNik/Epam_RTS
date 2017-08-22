bb9ba631c032ba5f62c6ef9008399382a14d65af
Svarog <admin@brainode.com>
2017-08-22 11:18:59 +0300

    Before moving minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Minimap : MonoBehaviour, IPointerClickHandler
+{
+    private Camera minimapCamera;
+    private Camera mainCamera;
+    private Vector3 pos;
+    private RawImage minimapImage;
+
+    void Start()
+    {
+        minimapCamera = GameObject.FindGameObjectWithTag("MinimapCamera").GetComponent<Camera>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        minimapImage = GetComponent<RawImage>();
+        Debug.Log(minimapImage.mainTexture.height);
+        Debug.Log(minimapImage.mainTexture.width);
+        Debug.Log("Camera height: " + minimapCamera.orthographicSize * 2);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+}
