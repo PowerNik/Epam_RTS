2d8ead26c503ebc87808d386830df84f5e496c3e
nmaschinskiy <nmaschinskiy@mail.ru>
2017-08-29 14:34:25 +0400

    Create able to add decorations (trees, rocks) at map

---
+++ Assets/Scripts/Map/Settings/DecorationSettingsSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DecorationSettings", menuName = "Map/Decoration Settings", order = 3)]
+public class DecorationSettingsSO : ScriptableObject
+{
+	[SerializeField]
+	private Decoration[] decorations;
+
+	public Decoration[] GetDecorations()
+	{
+		return decorations;
+	}
+}
