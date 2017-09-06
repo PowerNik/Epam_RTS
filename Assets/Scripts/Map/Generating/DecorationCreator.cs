ff194f51d04e6c4bbce8978511ca8ec9ae398f2c
nmaschinskiy <nmaschinskiy@mail.ru>
2017-09-06 16:20:28 +0400

    Create DecorationCreator.cs

---
+++ Assets/Scripts/Map/Generating/DecorationCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationCreator
+{
+	private DecorationSettings staticDecorSets;
+	private DecorationSettings dynamicDecorSets;
+
+	public void SetDecorationSettings(DecorationSettings staticDecorSets, DecorationSettings dynamicDecorSets)
+	{
+		this.staticDecorSets = staticDecorSets;
+		this.dynamicDecorSets = dynamicDecorSets;
+	}
+}
